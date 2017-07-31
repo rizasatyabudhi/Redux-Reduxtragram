6569c4d3b46aa2606edd586979d6f7ad89651320
Unknown <riza.ega.s@mail.ugm.ac.id>
2017-07-31 19:10:15 +0700

    Fixes route

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Single extends React.Component {
+  render() {
+    return (
+      <div className="single-photo">
+       I'm Single
+      </div>
+    );
+  }
+}
+
+export default Single;
