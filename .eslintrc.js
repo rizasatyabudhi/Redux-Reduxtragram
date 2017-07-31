780a31599039f3eeb66b3559e3fc6cb383541ca6
Unknown <riza.ega.s@mail.ugm.ac.id>
2017-07-31 17:45:54 +0700

    Add eslint airbnb

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {        
+        "linebreak-style" : 0,
+        "react/jsx-filename-extension" : 0
+
+    }
+};
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import CSS
+import css from './styles/style.styl';
+
+// Import Component
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
